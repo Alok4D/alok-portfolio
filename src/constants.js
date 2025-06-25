cbf8befb94b3b55b189010f0c3433640bd2adda5
Alok4D <alokroy602701@gmail.com>
2025-06-25 23:00:01 +0600

    skill section updated

--- src/constants.js
+++ src/constants.js
@@ -4,24 +4,24 @@ import cssLogo from "./assets/tech_logo/css.png";
 import sassLogo from "./assets/tech_logo/sass.png";
 import javascriptLogo from "./assets/tech_logo/javascript.png";
 import reactjsLogo from "./assets/tech_logo/reactjs.png";
-import angularLogo from "./assets/tech_logo/angular.png";
-import reduxLogo from "./assets/tech_logo/redux.png";
-import nextjsLogo from "./assets/tech_logo/nextjs.png";
+// import angularLogo from "./assets/tech_logo/angular.png";
+// import reduxLogo from "./assets/tech_logo/redux.png";
+// import nextjsLogo from "./assets/tech_logo/nextjs.png";
 import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
-import gsapLogo from "./assets/tech_logo/gsap.png";
+// import gsapLogo from "./assets/tech_logo/gsap.png";
 import materialuiLogo from "./assets/tech_logo/materialui.png";
 import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
-import springbootLogo from "./assets/tech_logo/springboot.png";
+// import springbootLogo from "./assets/tech_logo/springboot.png";
 import nodejsLogo from "./assets/tech_logo/nodejs.png";
 import expressjsLogo from "./assets/tech_logo/express.png";
-import mysqlLogo from "./assets/tech_logo/mysql.png";
+// import mysqlLogo from "./assets/tech_logo/mysql.png";
 import mongodbLogo from "./assets/tech_logo/mongodb.png";
 import firebaseLogo from "./assets/tech_logo/firebase.png";
 import cLogo from "./assets/tech_logo/c.png";
 import cppLogo from "./assets/tech_logo/cpp.png";
 import javaLogo from "./assets/tech_logo/java.png";
 import pythonLogo from "./assets/tech_logo/python.png";
-import typescriptLogo from "./assets/tech_logo/typescript.png";
+// import typescriptLogo from "./assets/tech_logo/typescript.png";
 import gitLogo from "./assets/tech_logo/git.png";
 import githubLogo from "./assets/tech_logo/github.png";
 import vscodeLogo from "./assets/tech_logo/vscode.png";
@@ -30,8 +30,8 @@ import mcLogo from "./assets/tech_logo/mc.png";
 import figmaLogo from "./assets/tech_logo/figma.png";
 import netlifyLogo from "./assets/tech_logo/netlify.png";
 import vercelLogo from "./assets/tech_logo/vercel.png";
-import postgreLogo from "./assets/tech_logo/postgre.png";
-import csharpLogo from "./assets/tech_logo/csharp.png";
+// import postgreLogo from "./assets/tech_logo/postgre.png";
+// import csharpLogo from "./assets/tech_logo/csharp.png";
 
 // Experience Section Logo's
 import webverseLogo from "./assets/company_logo/webverse_logo.png";
@@ -60,11 +60,11 @@ export const SkillsInfo = [
       { name: "SASS", logo: sassLogo },
       { name: "JavaScript", logo: javascriptLogo },
       { name: "React JS", logo: reactjsLogo },
-      { name: "Angular", logo: angularLogo },
-      { name: "Redux", logo: reduxLogo },
-      { name: "Next JS", logo: nextjsLogo },
+      // { name: "Angular", logo: angularLogo },
+      // { name: "Redux", logo: reduxLogo },
+      // { name: "Next JS", logo: nextjsLogo },
       { name: "Tailwind CSS", logo: tailwindcssLogo },
-      { name: "GSAP", logo: gsapLogo },
+      // { name: "GSAP", logo: gsapLogo },
       { name: "Material UI", logo: materialuiLogo },
       { name: "Bootstrap", logo: bootstrapLogo },
     ],
@@ -72,13 +72,13 @@ export const SkillsInfo = [
   {
     title: "Backend",
     skills: [
-      { name: "Springboot", logo: springbootLogo },
+      // { name: "Springboot", logo: springbootLogo },
       { name: "Node JS", logo: nodejsLogo },
       { name: "Express JS", logo: expressjsLogo },
-      { name: "MySQL", logo: mysqlLogo },
+      // { name: "MySQL", logo: mysqlLogo },
       { name: "MongoDB", logo: mongodbLogo },
       { name: "Firebase", logo: firebaseLogo },
-      { name: "PostgreSQL", logo: postgreLogo },
+      // { name: "PostgreSQL", logo: postgreLogo },
     ],
   },
   {
@@ -88,9 +88,10 @@ export const SkillsInfo = [
       { name: "C++", logo: cppLogo },
       { name: "Java", logo: javaLogo },
       { name: "Python", logo: pythonLogo },
-      { name: "C-Sharp", logo: csharpLogo },
+        { name: "Compass", logo: mcLogo },
+      // { name: "C-Sharp", logo: csharpLogo },
       { name: "JavaScript", logo: javascriptLogo },
-      { name: "TypeScript", logo: typescriptLogo },
+      // { name: "TypeScript", logo: typescriptLogo },
     ],
   },
   {
@@ -100,7 +101,7 @@ export const SkillsInfo = [
       { name: "GitHub", logo: githubLogo },
       { name: "VS Code", logo: vscodeLogo },
       { name: "Postman", logo: postmanLogo },
-      { name: "Compass", logo: mcLogo },
+    
       { name: "Vercel", logo: vercelLogo },
       { name: "Netlify", logo: netlifyLogo },
       { name: "Figma", logo: figmaLogo },
